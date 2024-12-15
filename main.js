cc8949821aace9a443629c8a51d365a27b51f6fb
Navin <navindena2432000@gmail.com>
2024-12-15 17:18:58 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,27 @@ app.get('/', (req, res) => {
     res.json({ msg : "Hello world"})
 });
 
+//crud functionality for movies
+//get results 
+app.get('/movies', () => {
+
+})
+
+//create movies
+app.post('/movies', () => {
+    
+})
+
+//updating movies
+app.put('/movies/:id', () => {
+    
+})
+
+//Delete movies
+app.delete('/movies/:id', () => {
+    
+})
+
 app.listen(PORT, () => {
     console.log(`this server is running at http://localhost:${8888}`);
 })
